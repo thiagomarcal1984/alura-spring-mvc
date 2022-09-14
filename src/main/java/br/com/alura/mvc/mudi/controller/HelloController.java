91af6e10f6ce1b261ac70280312e765eda7408dc
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-09-13 23:00:28 -0300

    O primeiro Controller.

---
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -0,0 +1,20 @@
+package br.com.alura.mvc.mudi.controller;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping(value = "hello")
+    public String hello(Model model, ServletRequest request) {
+        String nome = request.getParameter("nome");
+        if (nome == "" || nome == null)
+            nome = "Mundo";
+        model.addAttribute("nome", nome);
+        return "hello";
+    }    
+}
