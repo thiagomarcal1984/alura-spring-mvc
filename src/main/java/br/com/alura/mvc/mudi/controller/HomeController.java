4386764d7fce57f569d4895477cb9d5b22ea3daa
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-09-14 21:11:01 -0300

    Percorrendo a lista na view.

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,12 +1,32 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
     @GetMapping(value = {"/", "/home"})
-    public String home() {
+    public String home(Model model) {
+        Pedido pedido = new Pedido();
+        pedido.setNomeProduto("Xiamo Redmi Note 8");
+        pedido.setUrlImagem("https://m.media-amazon.com/images/I/51RIqTiRc8L._AC_SX679_.jpg");
+        pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-Camera/dp/B07Z5BBG56");
+        pedido.setDescricao("Uma descrição qualquer para o pedido.");
+
+        pedido.setValorNegociado(new BigDecimal("99999.99"));
+        pedido.setDataDaEntrega(LocalDate.parse("2022-01-31"));
+
+        List<Pedido> pedidos = Arrays.asList(pedido);
+        model.addAttribute("pedidos", pedidos);
+
         return "home";
     }
 }
---
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,61 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+    private String nomeProduto;
+    private BigDecimal valorNegociado;
+    private LocalDate dataDaEntrega;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+
+    public String getNomeProduto() {
+        return this.nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public BigDecimal getValorNegociado() {
+        return this.valorNegociado;
+    }
+
+    public void setValorNegociado(BigDecimal valorNegociado) {
+        this.valorNegociado = valorNegociado;
+    }
+
+    public LocalDate getDataDaEntrega() {
+        return this.dataDaEntrega;
+    }
+
+    public void setDataDaEntrega(LocalDate dataDaEntrega) {
+        this.dataDaEntrega = dataDaEntrega;
+    }
+
+    public String getUrlProduto() {
+        return this.urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return this.urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
