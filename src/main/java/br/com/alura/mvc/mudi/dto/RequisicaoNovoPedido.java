de540d57f93550caaecfad9eb4c05d8a55e21118
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-09-16 19:26:48 -0300

    Salvando pedido.

---
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,51 @@
+package br.com.alura.mvc.mudi.dto;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+
+public class RequisicaoNovoPedido {
+    private String nomeProduto;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+
+    public String getNomeProduto() {
+        return this.nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getUrlProduto() {
+        return this.urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return this.urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Pedido toPedido() {
+        Pedido pedido = new Pedido();
+        pedido.setDescricao(descricao);
+        pedido.setNomeProduto(nomeProduto);
+        pedido.setUrlImagem(urlImagem);
+        pedido.setUrlProduto(urlProduto);
+        return pedido;
+    }
+}
