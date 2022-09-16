dbe11459b849eff9ec2c6a10ab5bd437b413b310
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-09-16 16:52:09 -0300

    Injeção de dependências.

---
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -0,0 +1,22 @@
+package br.com.alura.mvc.mudi.repository;
+
+import java.util.List;
+
+import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+
+@Repository
+public class PedidoRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Pedido> recuperaTodosOsPedidos() {
+        Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
+        return query.getResultList();
+    }    
+}
